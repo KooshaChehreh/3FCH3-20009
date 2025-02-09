74994c0cfc007ec8f526c71fba673e777dc938ef
kooshachehreh <koosha.bagherzadeh@gmail.com>
2025-02-09 14:06:17 +0330

    table model added

--- restaurant/order/models.py
+++ restaurant/order/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from utils import seats_number_validator
 
-# Create your models here.
+
+class Table(models.Model):
+    name = models.CharField(
+        max_length=64,
+        unique=True,
+        verbose_name="نام میز",
+    )
+    seats_number = models.IntegerField(
+        validators=[seats_number_validator],
+        verbose_name="تعداد صندلی",
+    )
+
+    TABLE_RESERVED = "R"  # غیر همکار
+    TABLE_AVAILABLE = "A"  # در حال ثبت نام و احرازهویت
+
+    TABLE_STATE_CHOICES = [
+        (TABLE_RESERVED, "رزرو شده"),
+        (TABLE_AVAILABLE, "در دسترس"),
+    ]
+    table_state = models.CharField(
+        max_length=1,
+        choices=TABLE_STATE_CHOICES,
+        default=TABLE_AVAILABLE,
+        verbose_name="وضعیت میز",
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True, db_index=True, verbose_name="ایجاد شده در"
+    )
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="بروز شده در")
+
+
+    class Meta:
+        verbose_name = "میز"
+        verbose_name_plural = "میزها"
+        ordering = ["-id"]
+
+    
--- restaurant/utils.py
+++ restaurant/utils.py
@@ -6,3 +6,9 @@ def phone_validator(phone: str) -> str:
     if not re.match(r"^09\d{9}$", phone):
         raise ValidationError("Phone number must contain only 11 digits and starts with 09")
     return phone
+
+
+def seats_number_validator(seats_number: int) -> int:
+    if seats_number < 4 or seats_number > 10:
+        raise ValidationError("Seats number must br between 4 and 10")
+    return seats_number
